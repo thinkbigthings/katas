8bdb0bdd7d1584c5fe471aa24b7db72b009143ca
Jason Young <thinkbigthings@gmail.com>
2014-01-30 19:47:24 -0500

    cleaned up primes unit test

--- src/test/java/org/thinkbigthings/katas/primes/PrimeSearchTest.java
+++ src/test/java/org/thinkbigthings/katas/primes/PrimeSearchTest.java
@@ -1,24 +1,38 @@
 package org.thinkbigthings.katas.primes;
 
-
-
-
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
-import org.thinkbigthings.katas.primes.PrimeSearch;
-import org.thinkbigthings.katas.primes.PrimeSearchSieveOfEratosthenes;
 
 public class PrimeSearchTest {
 
-    @Test
-    public void calculatePrimes() {
+    private final PrimeSearch<Integer> primeSearch = new PrimeSearchSieveOfEratosthenes();
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testArgumentMustBeAbove1() throws Exception {
+        primeSearch.findPrimesBelow(0);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testArgumentMustBeBelowMax() throws Exception {
+        primeSearch.findPrimesBelow(Integer.MAX_VALUE);
+    }
 
-        PrimeSearch<Integer> sieve = new PrimeSearchSieveOfEratosthenes();
+    @Test
+    public void calculateShouldFindSinglePrime() throws Exception {
+
+        List<Integer> knownPrimes = Arrays.asList(2);
+        List<Integer> foundPrimes = primeSearch.findPrimesBelow(2);
+
+        Assert.assertEquals(knownPrimes, foundPrimes);
+    }
+    
+    @Test
+    public void calculateShouldFindBelow20() throws Exception {
 
         List<Integer> knownPrimes = Arrays.asList(2,3,5,7,11,13,17,19);
-        List<Integer> foundPrimes = sieve.findPrimesBelow(20);
+        List<Integer> foundPrimes = primeSearch.findPrimesBelow(20);
 
         Assert.assertEquals(knownPrimes, foundPrimes);
     }
